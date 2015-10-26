a2e679cce6b52d1784181d4e30b8748f7da5dce5
Michael McMillan <email@michaelmcmillan.net>
2015-10-26 14:27:07 +0100

    made question able to take an answer

--- src/Question.php
+++ src/Question.php
@@ -4,6 +4,7 @@ namespace Valgomat;
 class Question
 {
     protected $text;
+    protected $answer;
 
     public function __construct($text)
     {
@@ -13,5 +14,15 @@ class Question
     public function getText()
     {
         return $this->text;
+    }
+
+    public function isAnswered()
+    {
+        return isset($this->answer);
+    }
+
+    public function answer(Answer $answer)
+    {
+        $this->answer = $answer;
     }
 }
--- test/QuestionTest.php
+++ test/QuestionTest.php
@@ -1,5 +1,10 @@
 <?php
 use \Valgomat\Question;
+use \Valgomat\Answer;
+
+class AnswerMock extends Answer {
+
+}
 
 class QuestionTest extends PHPUnit_Framework_TestCase {
 
@@ -13,6 +18,19 @@ class QuestionTest extends PHPUnit_Framework_TestCase {
         $text = 'Et spørsmål hvor den første bokstaven allerede er stor.';
         $question = new Question($text);
         $this->assertEquals($question->getText(), $text);
+    }
+
+    public function testQuestionDoesNotHaveAnAnswerByDefault()
+    {
+        $question = new Question('Hvor fornøyd er du med Venstre?');
+        $this->assertFalse($question->isAnswered());
+    }
+
+    public function testQuestionCanBeAnswered()
+    {
+        $question = new Question('Hvor fornøyd er du med Venstre?');
+        $question->answer(new AnswerMock());
+        $this->assertTrue($question->isAnswered());
     }
 
 }
