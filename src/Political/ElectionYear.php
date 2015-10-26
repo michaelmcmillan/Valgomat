0231b1998ec8d03b3cfbfe3026f010a69102f9d9
Michael McMillan <email@michaelmcmillan.net>
2015-10-26 11:52:49 +0100

    prefixed private methods with underscore

--- src/Political/ElectionYear.php
+++ src/Political/ElectionYear.php
@@ -9,10 +9,9 @@ class ElectionYear
     public function __construct($year = false) 
     {
         if ($year) {
-            $this->setElectionYear($year); 
-        }
-        else {
-            $this->setElectionYearToCurrentYear(); 
+            $this->_setElectionYear($year); 
+        } else {
+            $this->_setElectionYearToCurrentYear(); 
         }
     }
 
@@ -21,23 +20,22 @@ class ElectionYear
         return $this->election_year;
     }
     
-    private function isValid($election_year) 
+    private function _isValid($election_year) 
     {
         return is_int($election_year) && $election_year > 0;
     }
 
-    private function setElectionYear($year) 
+    private function _setElectionYear($year) 
     {
-        if ($this->isValid($year)) {
+        if ($this->_isValid($year)) {
             $this->election_year = $year; 
-        }
-        else {
+        } else {
             throw new \InvalidArgumentException('Year must be a positive integer.'); 
         }
     }
 
-    public function setElectionYearToCurrentYear() 
+    private function _setElectionYearToCurrentYear() 
     {
-         $this->setElectionYear((int) date('Y'));
+         $this->_setElectionYear((int) date('Y'));
     }
 }
