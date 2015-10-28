047e7ce9e77d9f1e026d00eb1d132703c507468d
Michael McMillan <email@michaelmcmillan.net>
2015-10-28 20:04:19 +0100

    importance factor added to answer

--- src/Answer.php
+++ src/Answer.php
@@ -3,5 +3,45 @@ namespace Valgomat;
 
 class Answer
 {
+    private $_agree;
+    private $_importance;
 
+    public function __construct($agree, $importance)
+    {
+        if ($this->_isValidAgreeFactor($agree)) {
+            throw new \InvalidArgumentException(
+                'Agree factor must be from 1 to 5.'
+            );
+        }
+
+        if ($this->_isValidImportanceFactor($importance)) {
+            throw new \InvalidArgumentException(
+                'Importance factor must be from 1 to 3.'
+            );
+        }
+    }
+
+    public function getAgreeFactor()
+    {
+        return $this->_agree;
+    }
+
+    public function getImportanceFactor()
+    {
+        return $this->_importance;
+    }
+
+    private function _isValidImportanceFactor($importance_factor)
+    {
+        return !is_int($importance_factor)
+            || $importance_factor < 1
+            || $importance_factor > 3;
+    }
+
+    private function _isValidAgreeFactor($agree_factor)
+    {
+        return !is_int($agree_factor)
+            || $agree_factor < 1 
+            || $agree_factor > 5;
+    }
 } 
---
+++ test/AnswerTest.php
@@ -0,0 +1,41 @@
+<?php
+use \Valgomat\Answer;
+
+class AnswerTest extends PHPUnit_Framework_TestCase {
+
+    public function testAnswerThrowsExceptionIfAgreementIsBelowOne() {
+        $this->setExpectedException('InvalidArgumentException', '1 to 5');
+        $answer = new Answer(-1, 1);
+    }
+
+    public function testAnswerThrowsExceptionIfAgreementIsAboveFive() {
+        $this->setExpectedException('InvalidArgumentException', '1 to 5');
+        $answer = new Answer(6, 1);
+    }
+
+    public function testAnswerThrowsExceptionIfAgreementIsAString() {
+        $this->setExpectedException('InvalidArgumentException', '1 to 5');
+        $answer = new Answer('1', 1);
+    }
+
+    public function testAnswerDoesNotThrowExceptionIfAgreementIsBetweenOneAndFive() {
+        for ($agree = 1; $agree <= 5; ++$agree)
+           new Answer($agree, 1); 
+    }
+
+    public function testAnswerThrowsExceptionIfImportanceIsBelowOne() {
+        $this->setExpectedException('InvalidArgumentException', '1 to 3');
+        $answer = new Answer(1, 0);          
+    }
+
+    public function testAnswerThrowsExceptionIfImportanceIsAboveThree() {
+        $this->setExpectedException('InvalidArgumentException', '1 to 3');
+        $answer = new Answer(1, 4);          
+    }
+
+    public function testAnswerDoesNotThrowExceptionIfImportanceIsBetweenOneAndThree() {
+        new Answer(1, 1); 
+        new Answer(1, 2); 
+        new Answer(1, 3); 
+    }
+}
