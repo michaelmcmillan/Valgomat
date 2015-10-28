dc1e8d5059da5169518369fa1e585ebb7655ad90
Michael McMillan <email@michaelmcmillan.net>
2015-10-28 22:48:01 +0100

    political position class implemented for determining closest party to a question with an answer

--- src/Election/PoliticalPosition.php
+++ src/Election/PoliticalPosition.php
@@ -5,4 +5,35 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 class PoliticalPosition
 {
 
+    private $_question;
+    private $_parties;
+
+    public function __construct($question, $parties)
+    {
+        $this->_question = $question;
+        $this->_parties = $parties;
+    }
+
+    private function _getAgreeFactorFromQuestion()
+    {
+        return $this->_question->getAnswer()->getAgreeFactor();
+    }
+
+    private function _thereExistsPartyThatHasAgreeFactor($agree_factor)
+    {
+        return array_key_exists($agree_factor, $this->_parties); 
+    }
+
+    private function _getPartyThatAgreesWithAnswer()
+    {
+        $agree_factor = $this->_getAgreeFactorFromQuestion();
+        if ($this->_thereExistsPartyThatHasAgreeFactor($agree_factor)) {
+            return $this->_parties[$agree_factor];
+        }    
+    }
+
+    public function leansTowards()
+    {
+        return $this->_getPartyThatAgreesWithAnswer();
+    }
 }
--- test/PoliticalPositionTest.php
+++ test/PoliticalPositionTest.php
@@ -7,13 +7,56 @@ use \Valgomat\Answer;
 class PoliticalPositionTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testPoliticalPositionLeansTowardsPartyThatAgrees()
+    public function createQuestionWithFiveAsAgreementFactor()
     {
         $question = new Question('Drilling i Vesterålen er ikke bra.');
-        $party = new PoliticalParty('Venstre');
         $answer = new Answer(5, 1);
         $question->answer($answer);
-        $position = new PoliticalPosition($question, [5 => $venstre]);
-        $this->assertEquals($position->leansMostlyTowards(), $venstre);
+        return $question;
+    }
+
+    public function createQuestionWithFourAsAgreementFactor()
+    {
+        $question = new Question('Det bør være mindre offentlig styring.');
+        $answer = new Answer(4, 1);
+        $question->answer($answer);
+        return $question;
+    }
+
+    public function createQuestionWithOneAsAgreementFactor()
+    {
+        $question = new Question('Drilling i Vesterålen er ikke bra.');
+        $answer = new Answer(1, 1);
+        $question->answer($answer);
+        return $question;
+    }
+
+    public function testPoliticalPositionLeansTowardsPartyThatAgreesAsMuch()
+    {
+        $question = $this->createQuestionWithFiveAsAgreementFactor();
+        $agreeing_party = new PoliticalParty('Venstre');
+        $position = new PoliticalPosition($question, [5 => $agreeing_party]);
+        $this->assertSame($position->leansTowards(), $agreeing_party);
+    }
+
+    public function testPoliticalPositionLeansTowardsPartyThatDisagreesAsMuch()
+    {
+        $question = $this->createQuestionWithOneAsAgreementFactor();
+        $disagreeing_party = new PoliticalParty('Fremskrittspartiet');
+        $position = new PoliticalPosition($question, [1 => $disagreeing_party]);
+        $this->assertSame($position->leansTowards(), $disagreeing_party);
+    }
+
+    public function testPoliticalPositionLeansTowardsNoPartyIfNoPartyAgreesAsMuch()
+    {
+        $question = $this->createQuestionWithOneAsAgreementFactor();
+        $position = new PoliticalPosition(
+            $question, [
+                5 => new PoliticalParty('Sosialistisk Venstreparti'),
+                4 => new PoliticalParty('Høyre'),
+                3 => new PoliticalParty('Rødt') 
+            ]
+        );
+        $this->assertSame($position->leansTowards(), null);
     }
 }
