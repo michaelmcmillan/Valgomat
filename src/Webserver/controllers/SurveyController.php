62370f2715db0a2011da68201e62b48f0cb2ff2f
Michael McMillan <email@michaelmcmillan.net>
2015-10-29 13:22:17 +0100

    dummy data for survey controller

--- src/Webserver/controllers/SurveyController.php
+++ src/Webserver/controllers/SurveyController.php
@@ -1,10 +1,17 @@
 <?php
 namespace Valgomat\Webserver\Controllers;
+use Valgomat\Question;
+use Valgomat\Election\PoliticalSurvey;
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 class SurveyController extends Controller
 {
     public function create()
     {
-        $this->response->setBody("hello");
+        $survey = new PoliticalSurvey();
+        $question = new Question('Norge trenger færre skatter.');
+        $survey->addQuestion($question);
+        $this->app->response->headers->set('Content-Type', 'application/json');
+        $this->app->response->setBody(json_encode($survey));
     }
 }
