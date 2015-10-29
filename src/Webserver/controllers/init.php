2101a4626c9b2057ff65a4c7b1ef51b5accd91a1
Michael McMillan <email@michaelmcmillan.net>
2015-10-29 13:03:33 +0100

    removed init file, use autoloader instead

--- src/Webserver/controllers/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Valgomat\Webserver\Controllers;
-require_once __DIR__ . '/../../../vendor/autoload.php';
-require 'Controller.php';
-require 'IndexController.php';
-require 'SurveyController.php';
