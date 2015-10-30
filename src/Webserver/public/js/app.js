483b4e5089e30ad2d24aec6171429b73dcde8e71
Michael McMillan <email@michaelmcmillan.net>
2015-10-30 10:17:29 +0100

    implemented some dummy questions and a slider for determing political opinion

---
+++ src/Webserver/public/js/app.js
@@ -0,0 +1,111 @@
+;(function() {
+    'use strict';
+    
+    window.db = window.db || {};
+
+    window.db.Valgomat = (function($){
+
+        var user = {
+            guessedMunicipality: undefined,
+            municipality: undefined,
+            gender: undefined,
+            age: undefined
+        }
+
+        var demographicQuestionElements = {
+            gender: $('.demographic.genders'),
+            municipality: $('.demographic.municipality') 
+        }
+
+        var politicalQuestionElements = {
+            first: $('.political.question')
+        }
+
+        var sliderImg = '/public/img/slider.png';
+        var sliderElement = document.getElementById('slider');
+        var slider = new SmileySlider(sliderElement, sliderImg)
+
+        function startGuessingMunicipality () {
+            var jqxhr = $.get('/geolocate', function() {
+            }).done(function(response) {
+                user.guessedMunicipality = response.municipality;
+            }).fail(function() {
+                alert('Ohoh');
+            })
+        }
+
+        function askForGender () {
+            demographicQuestionElements.gender.show();
+
+            var genderButtons = $('.gender.male, .gender.female');
+
+            function answer (event) {
+                user.gender = $(this).attr('value');
+                demographicQuestionElements.gender.fadeOut(function () {
+                    nextQuestion();
+                });
+            } 
+
+            genderButtons.click(answer);
+        };
+
+        function firstDummyQuestion() {
+            politicalQuestionElements.first.fadeIn(); 
+            slider.position(1/2);
+        }
+
+        function askForMunicipality() {
+            demographicQuestionElements.municipality.fadeIn();
+
+            var agree = $('.municipality .agree');
+            var disagree = $('.municipality .disagree');
+
+            if (user.guessedMunicipality !== undefined) {
+                $('#municipality').text(user.guessedMunicipality.name + ' kommune');
+            }
+
+            agree.click(function (event) {
+                user.municipality = user.guessedMunicipality;
+                demographicQuestionElements.municipality.fadeOut(function () {
+                    nextQuestion();
+                });
+            });
+                
+            disagree.click(function (event) {
+                alert('Her ville jeg implementert en lignende løsning som dere.');
+                nextQuestion();
+            });
+        }
+
+        var questions = [
+            askForGender,
+            askForMunicipality,
+            firstDummyQuestion
+        ];
+
+        function nextQuestion () {
+            var nextQuestion = questions.shift();    
+            nextQuestion();
+        }
+
+        function init () {
+            startGuessingMunicipality();
+            nextQuestion();
+        };
+
+        function destroy () {
+
+        };
+
+        return {
+            init: init,
+            reflow: init,
+            destroy: destroy
+        };
+
+    })(jQuery);
+})();
+
+$(document).ready(function() {
+    window.db.Valgomat.init();
+});
